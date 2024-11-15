e134d9e43f5f9f7ae97490ff64acfe1a03daf1e3
abhayramgarhia <abhayramgarhia131@gmail.com>
2024-11-16 01:04:41 +0530

    few models created

---
+++ backend/chds_food_delivery/apps/users/models.py
@@ -0,0 +1,122 @@
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser,BaseUserManager,PermissionsMixin
+from django.utils.translation import gettext_lazy as _
+
+
+
+class UserMangaer(BaseUserManager):
+    use_in_migrations = True
+    def create_user(self, email, password, confirm_password=None, **extra_fields):
+        if not email:
+            raise ValueError("The email field must be required")
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save()
+        return user
+
+    def create_superuser(self, email, password, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+        extra_fields.setdefault("is_active", True)
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser must have is_staff=True")
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser must have is_superuser=True")
+        return self.create_user(email, password, **extra_fields)
+
+class User(AbstractBaseUser,PermissionsMixin):
+    """
+        Custom User Model
+    """
+    full_name = models.CharField(_("User Full Name"), max_length=30)
+    email = models.EmailField(_("User Email"), unique=True)
+    mobile_number = models.CharField(_("User Mobile Number"),max_length=13)
+    created_at = models.DateTimeField(_("User Creation Date&Time"),auto_now_add=True)
+    is_active = models.BooleanField(_("User Active"), default=True)
+    is_staff = models.BooleanField(_("User Active"), default=False)
+    is_superuser = models.BooleanField(_("User Active"), default=False)
+    
+    objects = UserMangaer()
+    REQUIRED_FIELDS=["full_name","email","mobile_number"]
+    
+    class Meta:
+        ordering = ['id']
+        verbose_name = 'User'
+        verbose_name_plural = 'Users'
+        
+    def __str__(self)-> str:
+        return f"{self.full_name}"
+        
+        
+
+class UserAddress(models.Model):
+    """ 
+        Model for storing User Address
+    """
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="addresses")
+    street_address = models.CharField(_("Street Address"),max_length=255,null=True,blank=True)
+    city = models.CharField(_("City"),max_length=100, null=True,blank=True)
+    state = models.CharField(_("State"),max_length=100,null=True,blank=True)
+    country = models.CharField(_("Country"),max_length=100,null=True,blank=True)
+    postal_code = models.CharField(_("Postal Code"),max_length=20,null=True,blank=True)
+    is_primary = models.BooleanField(_("Primary Address"),default=False)
+    created_at = models.DateTimeField(_("Created"),auto_now_add=True)
+    updated_at = models.DateTimeField(_("Updated"),auto_now=True)
+
+    def save(self, *args, **kwargs):
+        if self.is_primary:
+            UserAddress.objects.filter(user=self.user).update(is_primary=False)
+        super().save(*args, **kwargs)
+        
+    class Meta:
+        ordering = ['id']
+        verbose_name = 'User Address'
+        verbose_name_plural = 'User Addresses'
+        
+
+    def __str__(self)-> str:
+        return f"{self.street_address}, {self.city}, {self.state}, {self.country} - {self.postal_code}"
+    
+    
+class UserProfile(models.model):
+    """ 
+        Model for storing user  extra data
+    """
+    user_image = models.ImageField(_("User Image") , upload_to="user_images",null=True,blank=True)
+    date_of_birth = models.DateField(_("User Date of birth"),null=True,blank=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    
+    class Meta:
+        verbose_name = "User Profile"
+        verbose_name_plural = "User Profiles"
+    
+    
+class UserCardDetails(models.Model):
+    """ 
+        Model for storing User Card Details 
+    """
+    PREFERRED_PAYMENT_CHOICES = [
+        ('cards', 'Cards'),
+        ('alipay', 'Alipay'),
+        ('wechat', 'WeChat'),
+        ('paypal', 'PayPal'),
+        ('wallet', 'Wallet'),
+    ]
+    card_number = models.CharField(_("Card Number"),max_length=16, unique=True)
+    card_fullname = models.CharField(_("Card Holder Name"),max_length=100)
+    card_cvv = models.CharField(_("Card cvv number"),max_length=4)
+    card_expiry = models.DateField(_("Card Expiry Date"),max_length=8)
+    preferred_payment = models.CharField(_("Payment Method"),max_length=10,choices=PREFERRED_PAYMENT_CHOICES,default='cards')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='payment_cards')
+    is_active = models.BooleanField(default=True)
+    
+    class Meta:
+        verbose_name = "Payment Card"
+        verbose_name_plural = "Payment Cards"
+        ordering = ['-card_expiry']  
+        unique_together = ('card_number', 'user')
+        
+    def __str__(self):
+        return f"{self.card_fullname}"
+    
--- backend/chds_food_delivery/chds_food_delivery/settings/base.py
+++ backend/chds_food_delivery/chds_food_delivery/settings/base.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -39,10 +40,11 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 ]
 
-apps_urls = [
-    "apps.user"
+APPS_URLS = [
+    "apps.users"
 ]
-INSTALLED_APPS+=apps_urls
+INSTALLED_APPS+= APPS_URLS
+AUTH_USER_MODEL = "users.User"
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -120,7 +122,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+MEDIA_URL = "/media/"
+
+MEDIA_ROOT = os.path.join(BASE_DIR,"media")
+STATIC_ROOT = os.path.join(BASE_DIR,"static")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
