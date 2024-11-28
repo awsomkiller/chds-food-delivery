33a8662ffb1bff2f19a4a31dbb38b9609f82fca4
Ayush Kumar <advisorayush@gmail.com>
2024-11-28 22:20:21 +0530

    webhook update

--- backend/chds_food_delivery/apps/orders/urls.py
+++ backend/chds_food_delivery/apps/orders/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from apps.orders.views import OrderCreateView
+from apps.orders.views import OrderCreateView, stripe_webhook
 
 
 urlpatterns = [
     path("create/", OrderCreateView.as_view(), name="Order-Create"),
+    path("stripe/webhook/", stripe_webhook, name="stripe-webhook"),
 ]
