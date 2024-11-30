ad7309ae6a948d28268ff2e0a0e6a17f61d5384b
Ayush Kumar <advisorayush@gmail.com>
2024-12-01 00:39:50 +0530

    fix:mada changes

--- frontend/src/components/FooterComponent.vue
+++ frontend/src/components/FooterComponent.vue
@@ -1,279 +1,181 @@
 <template>
-      <footer class="footer-section bg-title fix">
-      <div class="footer-widgets-wrapper">
+<footer class="footer-section bg-title fix">
+    <div class="footer-widgets-wrapper">
         <div class="shape1 float-bob-y d-none d-xxl-block">
-          <img src="@/assets/homepage/footerShape1_1.png" alt="shape" />
+            <img src="@/assets/homepage/footerShape1_1.png" alt="shape" />
         </div>
-        
+
         <div class="shape3 d-none d-xxl-block">
-          <img src="@/assets/homepage/footerShape1_3.png" alt="shape" />
+            <img src="@/assets/homepage/footerShape1_3.png" alt="shape" />
         </div>
         <div class="shape4 d-none d-xxl-block">
-          <img src="@/assets/homepage/footerShape1_4.png" alt="shape" />
+            <img src="@/assets/homepage/footerShape1_4.png" alt="shape" />
         </div>
         <div class="container p-lg-3 p-md-">
-          <div class="footer-top">
-            <div class="row gy-4">
-              <div class="col-lg-4">
-                <div class="fancy-box">
-                  <div class="item1">
-                    <i class="fa-solid fa-location-dot"></i>
-                  </div>
-                  <div class="item2">
-                    <h6>address</h6>
-                    <p>4648 Rocky Road Philadelphia</p>
-                  </div>
-                </div>
-              </div>
-              <div
-                class="col-lg-4 d-flex justify-content-start justify-content-lg-end"
-              >
-                <div class="fancy-box">
-                  <div class="item1"><i class="fa-solid fa-envelope"></i></div>
-                  <div class="item2">
-                    <h6>send email</h6>
-                    <p>info@exmple.com</p>
-                  </div>
-                </div>
-              </div>
-              <div
-                class="col-lg-4 d-flex justify-content-start justify-content-lg-end"
-              >
-                <div class="fancy-box">
-                  <div class="item1">
-                    <i class="fa-solid fa-phone"></i>
-                  </div>
-                  <div class="item2">
-                    <h6>call emergency</h6>
-                    <p>+88 0123 654 99</p>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-
-          <div class="row">
-            <div
-              class="col-xl-3 col-lg-4 col-md-6"
-              data-wow-delay=".2s"
-              style="
-                visibility: visible;
-                animation-delay: 0.2s;
-                animation-name: fadeInUp;
-              "
-            >
-              <div class="single-footer-widget">
-                <div class="widget-head mb-2">
-                  <a href="index.html">
-                    <img src="@/assets/homepage/CHDS logo White transparent.png" class="logo-white" alt="logo-img" />
-                  </a>
-                </div>
-                <div class="footer-content">
-                 
-                  <div class="social-icon d-flex align-items-center">
-                    <a href="#"><i class="fab fa-facebook-f"></i></a>
-                    <a href="#"><i class="fab fa-twitter"></i></a>
-                    <a href="#"><i class="fa-brands fa-linkedin-in"></i></a>
-                    <a href="#"><i class="fa-brands fa-youtube"></i></a>
-                  </div>
-                  
-                </div>
-              </div>
-            </div>
-
-            <div
-              class="col-xl-3 col-lg-4 col-md-6"
-              data-wow-delay=".2s"
-              style="
-                visibility: visible;
-                animation-delay: 0.2s;
-                animation-name: fadeInUp;
-              "
-            >
-              <div class="single-footer-widget">
-                
-                <div class="footer-content">
-                  <figure class="wpb_wrapper vc_figure">
-                    <div class="vc_single_image-wrapper   vc_box_border_grey">
-                      <img width="200" height="200" src="@/assets/CHDS-Wechat.webp">
+            <div class="footer-top">
+                <div class="row gy-4">
+                    <div class="col-lg-4">
+                        <div class="fancy-box">
+                            <div class="item1">
+                                <i class="fa-solid fa-location-dot"></i>
+                            </div>
+                            <div class="item2">
+                                <h6>address</h6>
+                                <p>4648 Rocky Road Philadelphia</p>
+                            </div>
+                        </div>
                     </div>
-                  </figure>
+                    <div class="col-lg-4 d-flex justify-content-start justify-content-lg-end">
+                        <div class="fancy-box">
+                            <div class="item1"><i class="fa-solid fa-envelope"></i></div>
+                            <div class="item2">
+                                <h6>send email</h6>
+                                <p>info@exmple.com</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 d-flex justify-content-start justify-content-lg-end">
+                        <div class="fancy-box">
+                            <div class="item1">
+                                <i class="fa-solid fa-phone"></i>
+                            </div>
+                            <div class="item2">
+                                <h6>call emergency</h6>
+                                <p>+88 0123 654 99</p>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-              </div>
             </div>
 
+            <div class="row">
+                <div class="col-xl-3 col-lg-4 col-md-6" data-wow-delay=".2s" style="
+                visibility: visible;
+                animation-delay: 0.2s;
+                animation-name: fadeInUp;
+              ">
+                    <div class="single-footer-widget">
+                        <div class="widget-head mb-2">
+                            <a href="index.html">
+                                <img src="@/assets/homepage/CHDS logo White transparent.png" class="logo-white" alt="logo-img" />
+                            </a>
+                        </div>
+                        <div class="footer-content">
 
-            <div
-              class="col-xl-3 col-lg-4 col-md-6 ps-xl-5"
-              data-wow-delay=".4s"
-              style="
+                            <div class="social-icon d-flex align-items-center">
+                                <a href="#"><i class="fab fa-facebook-f"></i></a>
+                                <a href="#"><i class="fab fa-twitter"></i></a>
+                                <a href="#"><i class="fa-brands fa-linkedin-in"></i></a>
+                                <a href="#"><i class="fa-brands fa-youtube"></i></a>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-xl-3 col-lg-4 col-md-6" data-wow-delay=".2s" style="
+                visibility: visible;
+                animation-delay: 0.2s;
+                animation-name: fadeInUp;
+              ">
+                    <div class="single-footer-widget">
+
+                        <div class="footer-content">
+                            <figure class="wpb_wrapper vc_figure">
+                                <div class="vc_single_image-wrapper   vc_box_border_grey">
+                                    <img width="200" height="200" src="@/assets/CHDS-Wechat.webp">
+                                </div>
+                            </figure>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-xl-3 col-lg-4 col-md-6 ps-xl-5" data-wow-delay=".4s" style="
                 visibility: visible;
                 animation-delay: 0.4s;
                 animation-name: fadeInUp;
-              "
-            >
-              <div class="single-footer-widget">
-                <div class="widget-head">
-                  <h3>Quick Links</h3>
+              ">
+                    <div class="single-footer-widget">
+                        <div class="widget-head">
+                            <h3>Quick Links</h3>
+                        </div>
+                        <ul class="list-area">
+                            <li>
+                                <router-link to="/">
+                                    <i class="fa-solid fa-angles-right"></i>
+                                    Home
+                                </router-link>
+                            </li>
+                            <li>
+                                <router-link to="/ordernow">
+                                    <i class="fa-solid fa-angles-right"></i>
+                                    Order Now
+                                </router-link>
+                            </li>
+                            <li>
+                                <router-link to="/ourstory">
+                                    <i class="fa-solid fa-angles-right"></i>
+                                    Our Story
+                                </router-link>
+                            </li>
+                            <li>
+                                <router-link to="/contact-us">
+                                    <i class="fa-solid fa-angles-right"></i>
+                                    Contact Us
+                                </router-link>
+                            </li>
+                        </ul>
+                    </div>
                 </div>
-                <ul class="list-area">
-                  <li>
-                    <a href="/">
-                      <i class="fa-solid fa-angles-right"></i>
-                      Home
-                    </a>
-                  </li>
-                  <li>
-                    <a href="/ordernow">
-                      <i class="fa-solid fa-angles-right"></i>
-                      Order Now
-                    </a>
-                  </li>
-                  <li>
-                    <a href="/ourstory">
-                      <i class="fa-solid fa-angles-right"></i>
-                      Our Story
-                    </a>
-                  </li>
-                  <li>
-                    <a href="/contact-us">
-                      <i class="fa-solid fa-angles-right"></i>
-                      Contact Us
-                    </a>
-                  </li>
-                </ul>
-              </div>
+                <div class="col-xl-3 col-lg-4 col-md-6 ps-xl-5" data-wow-delay=".4s" style="
+                visibility: visible;
+                animation-delay: 0.4s;
+                animation-name: fadeInUp;
+              ">
+                    <div class="single-footer-widget">
+                        <div class="widget-head">
+                            <h3>Available Timings</h3>
+                        </div>
+                        <ul class="list-area">
+                            <li class="mb-2">
+                                Monday | Wednesday | Saturday
+                            </li>
+                            <li>
+                                <span class="text-theme-color2"> 8am - 12am | 4pm - 7pm </span>
+                            </li>
+                        </ul>
+                        <form action="#" class="mt-4">
+                            <div class="form-control">
+                                <input class="email" type="email" placeholder="Your email address" />
+                                <button type="submit" class="submit-btn">
+                                    <i class="fa-solid fa-arrow-right-long"></i>
+                                </button>
+                            </div>
+                            <div class="form-control style2 mt-3">
+                                <input id="checkbox" name="checkbox" type="checkbox" />
+                                <label for="checkbox">I agree to the
+                                    <a href="">Privacy Policy. </a></label>
+                            </div>
+                        </form>
+                    </div>
+                </div>
             </div>
-            <!-- <div
-              class="col-xl-3 col-lg-4 col-md-6 ps-xl-5"
-              data-wow-delay=".4s"
-              style="
-                visibility: visible;
-                animation-delay: 0.4s;
-                animation-name: fadeInUp;
-              "
-            >
-              <div class="single-footer-widget">
-                <div class="widget-head">
-                  <h3>Our Menu</h3>
-                </div>
-                <ul class="list-area">
-                  <li>
-                    <a href="menu.html">
-                      <i class="fa-solid fa-angles-right"></i>
-                      Burger King
-                    </a>
-                  </li>
-                  <li>
-                    <a href="menu.html">
-                      <i class="fa-solid fa-angles-right"></i>
-                      Pizza king
-                    </a>
-                  </li>
-                  <li>
-                    <a href="menu.html">
-                      <i class="fa-solid fa-angles-right"></i>
-                      Fresh Food
-                    </a>
-                  </li>
-                  <li>
-                    <a href="menu.html">
-                      <i class="fa-solid fa-angles-right"></i>
-                      Vegetable
-                    </a>
-                  </li>
-                  <li>
-                    <a href="menu.html">
-                      <i class="fa-solid fa-angles-right"></i>
-                      Desserts
-                    </a>
-                  </li>
-                </ul>
-              </div>
-            </div> -->
-            <div
-              class="col-xl-3 col-lg-4 col-md-6 ps-xl-5"
-              data-wow-delay=".4s"
-              style="
-                visibility: visible;
-                animation-delay: 0.4s;
-                animation-name: fadeInUp;
-              "
-            >
-              <div class="single-footer-widget">
-                <div class="widget-head">
-                  <h3>Available Timings</h3>
-                </div>
-                <ul class="list-area">
-                  <li class="mb-2">
-                    Monday | Wednesday | Saturday
-                  </li>
-                  <li>
-                    <span class="text-theme-color2"> 8am - 12am | 4pm - 7pm </span>
-                  </li>
-                </ul>
-                <form action="#" class="mt-4">
-                  <div class="form-control">
-                    <input
-                      class="email"
-                      type="email"
-                      placeholder="Your email address"
-                    />
-                    <button type="submit" class="submit-btn">
-                      <i class="fa-solid fa-arrow-right-long"></i>
-                    </button>
-                  </div>
-                  <div class="form-control style2 mt-3">
-                    <input id="checkbox" name="checkbox" type="checkbox" />
-                    <label for="checkbox"
-                      >I agree to the
-                      <a href="">Privacy Policy. </a></label
-                    >
-                  </div>
-                </form>
-              </div>
-            </div>
-          </div>
         </div>
-      </div>
-      <div class="footer-bottom">
+    </div>
+    <div class="footer-bottom">
         <div class="container">
-          <div
-            class="footer-wrapper d-flex align-items-center justify-content-center"
-          >
-            <p
-              class="wow fadeInLeft mb-0"
-              data-wow-delay=".3s"
-              style="
+            <div class="footer-wrapper d-flex align-items-center justify-content-center">
+                <p class="wow fadeInLeft mb-0" data-wow-delay=".3s" style="
                 visibility: visible;
                 animation-delay: 0.3s;
                 animation-name: fadeInLeft;
-              "
-            >
-            <a href=""> Chi Hun Da Su</a> © 2024. All rights reserved. 
-            </p>
-            <!-- <ul
-              class="brand-logo wow fadeInRight"
-              data-wow-delay=".5s"
-              style="
-                visibility: visible;
-                animation-delay: 0.5s;
-                animation-name: fadeInRight;
-              "
-            >
-              <li>
-                <a class="text-white" href="contact.html">
-                  Terms &amp; Condition
-                </a>
-              </li>
-              <li>
-                <a class="text-white" href="contact.html"> Privacy Policy </a>
-              </li>
-            </ul> -->
-          </div>
+              ">
+                    <a href=""> Chi Hun Da Su</a> © 2024. All rights reserved.
+                </p>
+            </div>
         </div>
-      </div>
-    </footer>
+    </div>
+</footer>
 </template>
 
 <style>
