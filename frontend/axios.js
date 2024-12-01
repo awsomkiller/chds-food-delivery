046b0c0a0ab6277d4a5eff764c67adc0cd66e7a4
Ayush Kumar <advisorayush@gmail.com>
2024-12-02 01:56:15 +0530

    fix: added api fix

--- frontend/axios.js
+++ frontend/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { useAuthStore } from '@/stores/auth';
 
 const caxios = axios.create({
-  baseURL: 'http://localhost:8000/api',
+  baseURL: 'https://api.chds.com.au/ordernow/api',
   timeout: 5000,
 });
 
